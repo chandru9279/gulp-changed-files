e01d016f9f50a513f846794c80555578ea60b228
T Chandirasekar <T Chandirasekar>
2014-10-10 15:24:07 +0800

    Adding todos, and copied over latest version <TC>

--- index.js
+++ index.js
@@ -7,19 +7,38 @@ var fs = require('fs');
 var through = require('through2');
 'use strict';
 
-module.exports = function (file, opt) {
-    if (!file) throw new PluginError('gulp-concat', 'Missing file option for gulp-concat');
-    if (!opt) opt = {};
+module.exports = function (opt) {
+    if (!opt) throw new PluginError('gulp-changed-files', 'Missing mandatory options for gulp-changed-files');
+    if (!opt.targetDir) throw new PluginError('gulp-changed-files', 'Missing targetDir option for gulp-changed-files');
+    if (!opt.baseDir) throw new PluginError('gulp-changed-files', 'Missing baseDir option for gulp-changed-files');
 
-    function transform(file) {
+    function transform(file, enc, callback) {
+        var vinylFile = file.path;
+        var targetFile = file.path.replace(opt.baseDir, opt.targetDir);
+        // TODO: Log when debug
+
+        console.log(file.path)
+        console.log(opt.baseDir)
+        console.log(opt.targetDir)
+        console.log(targetFile)
         if (file.isNull()) return; // ignore
         if (file.isStream()) return this.emit('error', new PluginError('gulp-changed-files', 'Streaming not supported'));
-        var digest = crypto.createHash('sha1').update(file.contents).digest('hex');
-        console.log(digest);
+
+        // TODO: dont compare is file sizes differ
+
+        var vinylDigest = crypto.createHash('sha1').update(file.contents).digest('hex');
+        var targetDigest = crypto.createHash('sha1').update(fs.readFileSync(targetFile)).digest('hex');
+        if (vinylDigest === targetDigest) {
+            console.log('Not copying ' + vinylFile + ' to ' + targetFile)
+        } else {
+            console.log('Copying ' + vinylFile + ' to ' + targetFile)
+            this.push(file)
+        }
+        return callback();
     }
 
     function flush() {
     }
 
-    return through(transform, flush);
+    return through.obj(transform, flush);
 };
