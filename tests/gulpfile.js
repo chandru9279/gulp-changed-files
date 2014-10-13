cdc559f4e4d4ecf85ce0d0d15c26d89ad9c11480
Chandru <chandru9279+npm@gmail.com>
2014-10-13 07:36:42 +0530

    Manual Tests setup

---
+++ tests/gulpfile.js
@@ -0,0 +1,16 @@
+var changed = require('../')
+var gulp = require('gulp')
+
+gulp.task('clean', function() {
+    return gulp.src('./target', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('move', function() {
+    return gulp.src('./source', {read: false})
+        .pipe(changed({
+            baseDir:'./source',
+            targetDir:'./target'
+        }))
+        .pipe(gulp.dest('./target'));
+});
